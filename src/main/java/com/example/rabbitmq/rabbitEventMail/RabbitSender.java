524c5247d250d688d0a9271746730124a3c80567
paduck-96 <peterrabitt1234@naver.com>
2023-03-30 22:05:10 +0900

    fix: RabbitMQ를 통한 이메일 발송

--- src/main/java/com/example/rabbitmq/rabbitEventMail/RabbitSender.java
+++ src/main/java/com/example/rabbitmq/rabbitEventMail/RabbitSender.java
@@ -1,6 +1,8 @@
 package com.example.rabbitmq.rabbitEventMail;
 
 import com.example.rabbitmq.config.RabbitConfig;
+import com.example.rabbitmq.rabbitWebPush.RabbitWebPushDTO;
+import com.example.rabbitmq.rabbitWebPush.RabbitWebPushMessage;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
@@ -16,5 +18,11 @@ public class RabbitSender {
     // exchange에 해 당 key 로 전달할 메시지 묶기
     public void sendEmailInfo(RabbitEmailMessage message) {
         rabbitTemplate.convertAndSend(RabbitConfig.RABBIT_JOIN_EXCHANGE, RabbitConfig.RABBIT_JOIN_ROUTING, message);
+    }
+
+    // web push sender
+    public void sendPush(RabbitWebPushMessage message){
+        rabbitTemplate.convertAndSend(RabbitConfig.RABBIT_PUSH_EXCHANGE,
+                RabbitConfig.RABBIT_PUSH_ROUTING, message);
     }
 }
